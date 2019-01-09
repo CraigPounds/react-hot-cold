47ca76428336609a4e32cda77172dd687a15f325
Craig Pounds <nweaver@windstream.net>
2019-01-08 20:34:04 -0600

    Basic smoke-tests for components

--- src/components/status-section.js
+++ src/components/status-section.js
@@ -7,7 +7,7 @@ import AuralStatus from './aural-status';
 export default function StatusSection(props) {
   const { guesses, auralStatus } = props;
   const guessCount = guesses.length;
-
+  console.log('guesses', guesses, 'auralStatus', auralStatus);
   return (
     <section aria-labelledby="guessCount" aria-describedby="guessList">
       <GuessCount guessCount={guessCount} />
